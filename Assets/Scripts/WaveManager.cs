52eae3e36217e6c89a5be18187a1b44c624fe010
rkd0124 <rkdgpwls3225@naver.com>
2025-12-05 02:37:31 +0900

    소환

--- Assets/Scripts/WaveManager.cs
+++ Assets/Scripts/WaveManager.cs
@@ -7,25 +7,25 @@ public class WaveManager : MonoBehaviour
     [System.Serializable]
     public class WaveData
     {
-        public int TkillerCount;
-        public int NKCount;
-        public int SuziSangCount;
-        public float spawnInterval;          // 소환 쿨타임
+        public int TkillerCount; //소환 목표치
+        public int NKCount; //소환 목표치
+        public int SuziSangCount; //소환 목표치
+        public float spawnInterval; // 소환 쿨타임
     }
 
     public EnemySpawner spawner; //적 스포너
     public List<WaveData> waves; //웨이브 정보
-    public int currentWave = 0;
+    public int currentWave = 0; //진행중인 웨이브 정보
 
-    private int enemiesToSpawn;
-    private int enemiesAlive;
+    private int enemiesToSpawn; // 이번 웨이브에 나올 총 적의 수
+    private int enemiesAlive; //살아있는 적의 수
 
     public Tower tower;               // 타워 체력 회복
-    public ResourceManager resources; // 폭탄/얼음/무기 충전
+    public ResourceManager resources; // 폭탄/얼음 충전
 
     public Material[] backgroundMaterials;   // 웨이브별 맵 텍스처
 
-    public Renderer mapRenderer;
+    public Renderer mapRenderer;// 텍스쳐를 적용할 배경
 
     // Start is called before the first frame update
     void Start()
@@ -40,48 +40,49 @@ public class WaveManager : MonoBehaviour
         
     }
 
-    public void StartWave(int waveIndex)
+    public void StartWave(int waveIndex) //웨이브 시작!
     {
-        currentWave = waveIndex;
-        WaveData data = waves[waveIndex];
+        currentWave = waveIndex; // 현재 웨이브 번호 갱신
+        WaveData data = waves[waveIndex]; // 리스트에서 웨이브의 정보 가져오기
 
         // 배경 텍스처 변경
         mapRenderer.material = backgroundMaterials[waveIndex];
 
         // 적 총합 계산
         enemiesToSpawn = data.TkillerCount + data.NKCount + data.SuziSangCount;
-        enemiesAlive = enemiesToSpawn;
+        enemiesAlive = enemiesToSpawn; // 살아있는 적을 적 총합으로 변경
 
-        StartCoroutine(RunWave(data));
+        StartCoroutine(RunWave(data)); //적 소환
     }
 
     IEnumerator RunWave(WaveData data)
     {
-        int t = data.TkillerCount;
-        int n = data.NKCount;
-        int s = data.SuziSangCount;
+        int t = data.TkillerCount; // 소환 목표치 : tkiller세포
+        int n = data.NKCount; // 소환 목표치 : tkiller세포
+        int s = data.SuziSangCount; // 소환 목표치 : tkiller세포
 
-        while (t > 0 || n > 0 || s > 0)
+        while (t > 0 || n > 0 || s > 0)// 셋중 하나라도 목표치 미달이면
         {
             if (t > 0)
             {
-                spawner.Spawn("Tkiller");
+                spawner.Spawn("Tkiller"); //소환
                 t--;
             }
 
             if (n > 0)
             {
-                spawner.Spawn("NK");
+                spawner.Spawn("NK"); //소환
                 n--;
             }
 
             if (s > 0)
             {
-                spawner.Spawn("SuziSang");
+                spawner.Spawn("SuziSang"); //소환
                 s--;
             }
 
             yield return new WaitForSeconds(data.spawnInterval);
+            //소환 쿨타임
         }
     }
 
