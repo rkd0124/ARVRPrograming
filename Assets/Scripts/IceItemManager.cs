ce524297bc71f3e3c18d9c2c8bd60801f33c3b67
fefef <fef>
2025-12-08 11:20:39 +0900

    얼음 발동 X키로 변경

--- Assets/Scripts/IceItemManager.cs
+++ Assets/Scripts/IceItemManager.cs
@@ -20,7 +20,7 @@ public class IceItemManager : MonoBehaviour
     // Update is called once per frame
     void Update() // Q키 입력 감지
     {
-        if (Input.GetKeyDown(KeyCode.Q))   // Q 버튼
+        if (ARAVRInput.GetDown(ARAVRInput.Button.Three, ARAVRInput.Controller.LTouch))   // X 버튼
         {
             Debug.Log(currentGauge);
             ActivateIceItem();             // 얼음 아이템 발동
