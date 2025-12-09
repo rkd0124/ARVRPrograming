9199af7d24638fb6fc2da2a484b4112149ffa2ff
rkd0124 <rkdgpwls3225@naver.com>
2025-12-10 01:08:37 +0900

    폭탄생성기 3초동안 보면 폭탄 소환

--- Assets/Scripts/Player_BombLauncher.cs
+++ Assets/Scripts/Player_BombLauncher.cs
@@ -12,16 +12,31 @@ public class Player_BombLauncher : MonoBehaviour
     public float cooldown = 1.5f; // 쿨타임
     private bool isCooldown = false; // 쿨타임 상태
 
+    public float interactionDistance = 10f; // 상호작용 가능한 거리
+    public float requiredLookTime = 3.0f; // 바라봐야 하는 시간 (3초)
+    private float lookTimer = 0f; // 현재 바라본 시간
+    private Transform mainCameraTransform; // 메인 카메라 위치
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (Camera.main != null)
+        {
+            mainCameraTransform = Camera.main.transform;
+        }
+
+        else
+        {
+            Debug.LogError("Main Camera가 씬에 없습니다!");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        // E키 입력
+
+        CheckBombMaker(); // 폭탄 바라보기
+
         if (Input.GetKeyDown(KeyCode.E))
         {
             AttemptThrow();
@@ -46,6 +61,31 @@ public class Player_BombLauncher : MonoBehaviour
 
         SpawnBomb();
     }
+
+    void CheckBombMaker()
+    {
+        Ray ray = new Ray(mainCameraTransform.position, mainCameraTransform.forward);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, interactionDistance))
+        {
+            // 부딪힌 물체의 이름이 "BombMaker" 인지 확인
+            if (hit.collider.CompareTag("BombMaker"))
+            {
+                lookTimer += Time.deltaTime; //시간을 카운트
+                if (lookTimer >= requiredLookTime)
+                {
+                    AttemptThrow(); // 폭탄 생성(쿨타임 검증단계에ㅔ)
+                    lookTimer = 0f; // 타이머 초기화 (연속 리필 방지)
+                }
+                return; // 함수 종료 (아래 초기화 코드 실행 안 함)
+            }
+        }
+
+        // BombMaker를 안 보고 있거나, 허공을 보면 타이머 초기화
+        lookTimer = 0f;
+    }
+
 
     void SpawnBomb()
     {
